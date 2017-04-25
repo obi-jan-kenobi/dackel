63aef46e200714563871c18338ce6e41276efd97
ifartglitter <j.sziegaud@gmail.com>
2017-04-25 22:42:14 +0200

    initial commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+'use strict'
+
+const request = require('request')
+
+function subscribe(uri, options, cb) {
+  if (typeof options === 'function') cb = options
+  const interval = setInterval(() => {
+    request(uri, options, cb)
+  }, options.interval || 5000)
+  return function unsubscribe() {
+    clearInterval(interval)
+  }
+}
+
+module.exports = subscribe
