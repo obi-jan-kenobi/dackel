2d91f1dfbda96c6baeeccd269ab4ebf7c429ea5c
Jan Sziegaud <j.sziegaud@gmail.com>
2017-04-26 18:38:07 +0200

    switched to jest & added more tests

--- test/index.test.js
+++ test/index.test.js
@@ -1,21 +1,38 @@
 'use strict'
-
-const expect = require('chai').expect
+jest.mock('request')
 const subscribe = require('../src')
 
 describe('subscribe', () => {
   it('should return a function', () => {
-    expect(subscribe('any', () => unsubscribe())).to.be.a('function')
+    expect(subscribe('any', () => unsubscribe())).toBeInstanceOf(Function)
   })  
-  it('should make requests', done => {    
+  it('should make requests', done => {
+    const request = require('request')
+    request.mockImplementation((uri, options, cb) => cb(1))
     const unsubscribe = subscribe('https://api.github.com/users/obi-jan-kenobi', {
-      interval: 1500,
+      interval: 5,
       headers: {
         'User-Agent': 'request'
       }}, (err, response, body) => {
-      expect(!err).to.be.ok
+      expect(request).toBeCalled()
       unsubscribe()
       done()
     })
-  }).timeout(2500)
+  })
+  it('should make several requests', done => {
+    const request = require('request')
+    let counter = 0
+    request.mockImplementation((uri, options, cb) => cb(++counter))
+    const unsubscribe = subscribe('https://api.github.com/users/obi-jan-kenobi', {
+      interval: 1,
+      headers: {
+        'User-Agent': 'request'
+      }}, (err, response, body) => {
+    })
+    setTimeout(() => {
+      expect(counter).toBeGreaterThanOrEqual(3)
+      unsubscribe()
+      done()
+    }, 6)
+  })
 })
